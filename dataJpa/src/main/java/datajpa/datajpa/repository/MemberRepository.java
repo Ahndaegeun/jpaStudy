a0813bcffa53161a76e38e5ed2f424396e220b79
ahndaegeun <adg0807@naver.com>
2022-02-27 20:14:42 +0900

    entity Setting

---
+++ dataJpa/src/main/java/datajpa/datajpa/repository/MemberRepository.java
@@ -0,0 +1,9 @@
+package datajpa.datajpa.repository;
+
+import datajpa.datajpa.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+}
